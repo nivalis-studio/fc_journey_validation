bc59d7aae820e7c01684dd2377a7c896e95decf6
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-06-13 20:10:40 +0200

    chore: refactor gps trace operations into methods

--- src/traces_validation.rs
+++ src/traces_validation.rs
@@ -3,40 +3,12 @@ use anyhow::Result;
 use geo::ChaikinSmoothing;
 use geo::FrechetDistance;
 use geo::HaversineLength;
-use geo::OutlierDetection;
-use geo::{
-    coord, DensifyHaversine, LineInterpolatePoint, LineString, Point, RemoveRepeatedPoints,
-    Simplify,
-};
+use geo::{LineString, RemoveRepeatedPoints, Simplify};
 
 use crate::traces_to_geojson::traces_to_geojson;
 
 pub fn traces_validation(t1: GpsTrace, t2: GpsTrace) -> Result<(geo::LineString, geo::LineString)> {
-    let line_strings: Vec<LineString<f64>> = [t1, t2]
-        .iter()
-        .map(|t| {
-            let coords: Vec<Point> = t.points.iter().map(Point::from).collect();
-
-            let filtered_coords: Vec<_> = coords
-                .iter()
-                .zip(coords.outliers(3).iter())
-                .filter(|(_, &score)| score <= 1.0) // Adjust threshold as needed
-                .map(|(&point, _)| point)
-                .collect();
-
-            let line_string = LineString(
-                filtered_coords
-                    .iter()
-                    .map(|p| coord! { x: p.x(), y: p.y() })
-                    .collect::<Vec<_>>(),
-            );
-
-            line_string.densify_haversine(0.1);
-            line_string.line_interpolate_point(0.1);
-
-            line_string
-        })
-        .collect();
+    let line_strings: Vec<LineString<f64>> = [t1, t2].iter().map(|t| t.to_line_string()).collect();
 
     let line_string1 = &line_strings[0];
     let line_string2 = &line_strings[1];
