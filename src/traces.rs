2dee4f8053a8ab86a9231975b55dbd6ffeae75d9
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-06-15 21:42:38 +0200

    chore: cleanup geojson visualization

--- src/traces.rs
+++ src/traces.rs
@@ -1,14 +1,12 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::VecDeque;
 use std::ops::Deref;
 
-use anyhow::Context;
 use chrono::{DateTime, Utc};
 use geo::{
     coord, Closest, Coord, DensifyHaversine, FrechetDistance, HaversineClosestPoint,
     HaversineDistance, HaversineLength, OutlierDetection, RemoveRepeatedPoints, Simplify,
 };
 use geo::{LineString, Point};
-use geojson::{Feature, FeatureCollection, Geometry, JsonObject, JsonValue};
 use serde::{Deserialize, Serialize};
 
 use crate::error::JourneyValidationError;
@@ -175,66 +173,6 @@ impl GpsTracesPair {
         let (driver, passenger) = self.to_traces();
 
         (driver.simplified(), passenger.simplified())
-    }
-
-    pub fn to_geojson(&self) -> FeatureCollection {
-        let (driver_trace, passenger_trace) = self.to_simplified_traces();
-
-        let create_properties = |color: &str, width: &str, opacity: &str| -> Option<JsonObject> {
-            let mut properties = JsonObject::new();
-            let properties_: HashMap<String, JsonValue> = [
-                ("stroke".to_string(), JsonValue::from(color)),
-                ("stroke-width".to_string(), JsonValue::from(width)),
-                ("stroke-opacity".to_string(), JsonValue::from(opacity)),
-            ]
-            .iter()
-            .cloned()
-            .collect();
-
-            properties.extend(properties_);
-
-            Some(properties)
-        };
-
-        FeatureCollection {
-            bbox: None,
-            features: vec![
-                Feature {
-                    bbox: None,
-                    geometry: Some(Geometry {
-                        value: geojson::Value::from(&*driver_trace),
-                        bbox: None,
-                        foreign_members: None,
-                    }),
-                    id: None,
-                    properties: create_properties("#00a3d7", "2", "1"),
-                    foreign_members: None,
-                },
-                Feature {
-                    bbox: None,
-                    geometry: Some(Geometry {
-                        value: geojson::Value::from(&*passenger_trace),
-                        bbox: None,
-                        foreign_members: None,
-                    }),
-                    id: None,
-                    properties: create_properties("#ff6251", "2", "1"),
-                    foreign_members: None,
-                },
-            ],
-            foreign_members: None,
-        }
-    }
-
-    pub fn visualize(&self) -> anyhow::Result<()> {
-        let geojson = self.to_geojson().to_string();
-
-        let uri_data = urlencoding::encode(&geojson);
-        let url = format!("http://geojson.io/#data=data:application/json,{}", uri_data);
-
-        open::that(url).context("Failed to open geojson in the default browser")?;
-
-        Ok(())
     }
 }
 
