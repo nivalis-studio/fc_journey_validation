a3594ad31d3694c6b2989cfff12544b192cad0bf
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-06-17 14:20:17 +0200

    chore: cleanup

--- src/cli.rs
+++
@@ -1,9 +0,0 @@
-use std::path::PathBuf;
-
-use clap::Parser;
-
-#[derive(Parser)]
-pub struct Cli {
-    #[arg(short, long)]
-    pub file_path: Option<PathBuf>,
-}
--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod cli;
-pub mod error;
-pub mod journeys;
-pub mod output;
-pub mod points;
-pub mod traces;
-
-pub type Result<T, E = error::JourneyValidationError> = std::result::Result<T, E>;
