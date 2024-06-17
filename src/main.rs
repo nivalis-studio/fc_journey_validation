0185c196e0f4bf7321076e3df7a9ae062db474c5
Pierre-Nicolas Watin-Augouard <pnwa@icloud.com>
2024-06-17 20:48:34 +0200

    feat: add output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::Parser;
+use gps_trajectory_validation::{cli::Cli, input::JourneyInput, journey::Journey, output::Output};
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let journey_result = match cli.file_path {
+        Some(path) => JourneyInput::try_from(path),
+        None => JourneyInput::from_stdin(),
+    }
+    .and_then(Journey::try_from);
+
+    let output = match journey_result {
+        Ok(journey) => journey.validate(),
+        Err(err) => Output::from(err),
+    };
+
+    let output_json = serde_json::to_string(&output).unwrap();
+
+    let mut stdout = io::stdout();
+
+    write!(stdout, "{}", output_json).unwrap();
 }
---
+++ src/output.rs
@@ -0,0 +1,62 @@
+use chrono::{DateTime, Utc};
+use serde::Serialize;
+use serde_with::skip_serializing_none;
+
+use crate::error::JourneyValidationError;
+
+#[skip_serializing_none]
+#[derive(Serialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub struct OutputError {
+    pub cancel_reason: String,
+}
+
+#[skip_serializing_none]
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct OutputSuccess {
+    pub common_distance: f64,
+    pub common_start_point: PointOutput,
+    pub common_end_point: PointOutput,
+    pub average_confidence: f64,
+    pub traces: TracesOutput,
+}
+
+#[allow(clippy::large_enum_variant)]
+#[skip_serializing_none]
+#[derive(Serialize, Default)]
+#[serde(rename_all = "camelCase")]
+pub enum Output {
+    Error(OutputError),
+    Success(OutputSuccess),
+    #[default]
+    Empty,
+}
+
+#[derive(Serialize)]
+pub struct TracesOutput {
+    pub driver_trace: TraceOutput,
+    pub passenger_trace: TraceOutput,
+}
+
+#[derive(Serialize)]
+pub struct TraceOutput {
+    pub id: String,
+    pub points: Vec<String>,
+}
+
+#[derive(Serialize, Clone)]
+pub struct PointOutput {
+    pub id: String,
+    pub timestamp: DateTime<Utc>,
+    pub latitude: f64,
+    pub longitude: f64,
+}
+
+impl From<JourneyValidationError> for Output {
+    fn from(value: JourneyValidationError) -> Self {
+        Self::Error(OutputError {
+            cancel_reason: value.to_string(),
+        })
+    }
+}
